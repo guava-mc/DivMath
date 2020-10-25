ba6fef1d1f9881f53df89be2fb4b04dc568eddea
Mason Cole <mcole18@asu.edu>
2020-10-25 02:18:38 -0700

    DivHacks

---
+++ div-math/README.md
@@ -0,0 +1,32 @@
+## Inspiration
+A good "numbersense", which is an understanding of fundamental math concepts, provides the foundation for success in formal math education. Children who enter kindergarten with a good mathematical foundation perform better than their counterparts with a lower comprehension of basic math concepts and that this level of performance is a good indicator of future success (or struggle) with math throughout a child's education.
+
+Children with a lower socio-economic status (SES), are not equipped with a strong grasp of math fundamentals before entering school resulting in poor performance on math assessments when compared to other countries and their higher SES peers. Furthermore, many gender biases surround math and STEM topics that creates a divide in preparedness of children based on gender. Ultimately these socio-econominc and socio-cultural factors contribute to under representation of low SES groups and females in STEM jobs.
+
+These children do not have enough focused attention on learning math or access to resources needed to establish a firm grasp of math fundamentals prior to entering school, resulting in an inexperience with math fundamentals, few strategies to learn math, and poor performance.
+
+Early math learning begins in the home and a child's primary 'teacher' is their caregiver/parent. However, components in early math learning that are lacking for under represented groups includes parents who lack a solid math foundation, parents who do not emphasize math as part of early learning, no equity of access to relatable learning materials, lack of metacognitive skills in both parents and their children.
+
+This DivHack aims to address this problem, even the playing field, and increase representation in STEM.
+
+## What it does
+This hack digitizes a board game that has been proven by Ramani and Siegler in their paper "How Informal Learning Activities Can Promote Children’s Numerical Knowledge" to improve math fundamentals through informal play with virtual scaffolding.
+
+With the average child spending over 2 hours a week using touchscreens and the ubiquity of touchscreens a digital game with a virtual agent that can provide guidance and reinforce metacognitve skills and a growth mindset can better prepare children for success in mathematics, which is correlated with better achievement overall in school and more career opportunities later in life.
+
+## How I built it
+- frontend: ReactJS, bootstrap
+- backend: Ruby on Rails
+- research into early math development and digital games
+
+## Challenges I ran into
+Solo hacking a game that delivers a high UX and usability for preschool age children took a lot of planning. Implementing that interface was my biggest challenge.
+
+## Accomplishments that I'm proud of
+I think the concept and basic workflow are an amazing foundation to build from to get this game in the wild and increase diversity in STEM.
+
+## What I learned
+A good idea demands good design and implementation. All three are necessary ingredients when attempting to deliver a quality product.
+
+## What's next for DivMath
+Improve the UX. Integrate a parent portal and assessments to track progress. Add adaptive and personalized learning opportunities. Add more games for a more well rounded foundation.
---
+++ div-math/src/App.js
@@ -0,0 +1,158 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SettingsIcon from '@material-ui/icons/Settings';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import Paper from '@material-ui/core/Paper';
+import { Spring } from "react-spring/renderprops-universal";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+    textAlign: 'center',
+    fontFamily: 'Nunito',
+    fontSize: 70,
+  },
+  control: {
+    padding: theme.spacing(0),
+  },
+}));
+
+const paperColor = ['#cc4125ff', '#f6b26bff', '#ffd966ff', '#93c47dff', '#3c78d8ff', '#9900ffff', ]
+
+export default function App() {
+  const [currentPaper, setPaper] = React.useState(1);
+  const [spacing, setSpacing] = React.useState(0);
+  const [spin, setSpin] = React.useState(3);
+  const [turn, setTurn] = React.useState(false);
+  const [deg, setDeg] = React.useState(0);
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    // setSpacing(Number(event.target.value));
+    // selectPaper(Number(event.target.value));
+    // alert(event);
+  };
+
+  const findSpinValue = () => {
+    if(spin != 0)
+      return setSpin(spin);
+    if(deg % 360 <= 90 && deg % 360 > 15)
+      return setSpin(1);
+    if(deg % 360 <= 90 && deg % 360 > 15)
+      return setSpin(2);
+    if(deg % 360 <= 135 && deg % 360 > 90)
+      return setSpin(3);
+    if(deg % 360 <= 195 && deg % 360 > 135)
+      return spin;
+    if(deg % 360 <= 270 && deg % 360 > 195)
+      setSpin(4);
+    else
+      return setSpin(5);
+  }
+  const toggle = () => {setTurn(!turn); setDeg(Math.floor(Math.random() * 360) + 360+ deg); console.log(deg); setSpin(findSpinValue)} ;
+
+  return (
+    <div><br/><br/>
+      <IconButton aria-label="delete">
+              <SettingsOutlinedIcon style={{marginLeft: 210}}/>
+      </IconButton>
+      <br/><br/>
+      {spin == 0 && <div style={{marginLeft: 210,
+        fontFamily: 'Nunito',
+        fontSize: 70,}}>spin the wheel to play...</div>}
+      {spin != 0 && <div style={{marginLeft: 210,
+        fontFamily: 'Nunito',
+        fontSize: 70,}}>move {spin} spaces</div>}
+    <br/><br/><br/><br/>
+    <Grid container className={classes.root} spacing={0}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={spacing}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+            <Grid key={value} item>
+              {currentPaper == value && <Paper style={{
+                background: paperColor[value % paperColor.length],
+                border: '5px solid yellow',
+                color: 'white'}} className={classes.paper}
+                                               onClick={() => {
+                                                 if(spin > 0 && value > currentPaper && value - (spin + currentPaper) <= 0) {
+                                                   setSpin(spin - (value - currentPaper));
+                                                   setPaper(value);
+                                                 }
+                                               }}>{value}
+              </Paper> }
+              {currentPaper != value && <Paper style={{
+                background: paperColor[value % paperColor.length],
+                color: 'white'}} className={classes.paper}
+                                               onClick={() => {
+                                                 if(spin > 0 && value > currentPaper && value - (spin + currentPaper) <= 0) {
+                                                   setSpin(spin - (value - currentPaper));
+                                                   setPaper(value);
+                                                 }
+                                                   }}>{value}
+              </Paper> }
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+      <div style={{marginLeft: 210, marginTop: 10,}}>
+      <Grid
+          container
+          className={classes.root}
+          direction="row"
+          alignItems="center"
+          spacing={100}
+      >
+        <Grid item xs={1}>
+        <img src={'arrow.png'} alt='arrow' />
+        </Grid>
+        <Grid item xs={2} style={{marginLeft: 200}}>
+      <Spring
+        from={{ rotation: "0deg" }}
+        to={{ rotation: turn ? "0" : deg + "%" }}
+        style={{marginLeft: 210}}
+    >
+
+        {({ rotation }) => (
+            <img src={'wheel.png'} alt='wheel' style={{ transform: `rotate(${rotation})` }} onClick={toggle}/>
+        )}
+      </Spring>
+        </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> asdfand save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
